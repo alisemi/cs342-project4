055e745b6b72a152e91441d8af505728f827e967
Ibrahim Korpeoglu <korpe@cs.bilkent.edu.tr>
2017-12-05 13:27:21 +0300

    did small modifications

--- myfs.h
+++ myfs.h
@@ -3,10 +3,10 @@
 
 #define BLOCKSIZE          4096     // bytes
 #define MAXFILECOUNT       128      // files
-#define DISKSIZE         (1<<28)  // 256 MB
-#define MAXFILENAMESIZE    128  // characters - max that FS can support
+#define DISKSIZE         (1<<27)  // 256 MB
+#define MAXFILENAMESIZE    32  // characters - max that FS can support
 #define BLOCKCOUNT      (DISKSIZE / BLOCKSIZE)
-#define MAXOPENFILES       128      // files
+#define MAXOPENFILES       64      // files
 #define MAXREADWRITE      1024     // bytes; max read/write amount
 
 // The following will be use to create and format a disk
@@ -15,7 +15,8 @@ int myfs_makefs (char *diskname);
 
 // The following will be used by a program to work with files
 int myfs_mount (char *vdisk);
-int myfs_umount();
+int myfs_umount();formatdisk.c
+
 int myfs_create(char *filename);
 int myfs_open(char *filename);
 int myfs_close(int fd);
